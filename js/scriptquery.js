af51053f6dd48c47b795ea2ce8658350f10359d9
Darisam <98291008+Darisam@users.noreply.github.com>
2022-03-14 09:20:58 +0100

    Create ability to add an item to the list

--- js/scriptquery.js
+++ js/scriptquery.js
@@ -1,2 +1,13 @@
 
+// Add a new item to the list
+
 let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.text(inputValue);
+
+if (inputValue === '') {
+  alert('You must write something!');
+} else {
+  $('#list').append(li);
+}
+
